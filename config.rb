dc7522691e6e9110a4576ebf666d7a05aa3c2953
Jean-Michel Bruel <jbruel@gmail.com>
2015-09-02 13:07:24 +0200

    Published gh-pages to GitHub pages.

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ project_type = :stand_alone
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "_site/stylesheets"
+css_dir = "_site/css"
 sass_dir = "stylesheets"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "_site/css"
 fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
