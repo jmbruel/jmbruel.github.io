8b55ce8f7ca03c4affd41338901cf48c6de92102
Jean-Michel Bruel <jbruel@gmail.com>
2015-09-22 23:47:53 +0200

    final erb

---
+++ _ext/pipeline.rb
@@ -0,0 +1,22 @@
+require 'zurb-foundation'
+require './stylesheets/_themes/lib/functions.rb'
+#require File.expand_path '../haml/filters/asciidoc.rb', __FILE__
+
+
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+#  helper Awestruct::Extensions::Partial
+
+#  extension Awestruct::Extensions::Indexifier.new
+
+#  extension Awestruct::Extensions::Publis.new '/publis'
+
+#  extension Awestruct::Extensions::Paginator.new :publis, '/index',
+#      :page_name => 'publis/page/', :per_page => 5
+
+#  extension Awestruct::Extensions::Atomizer.new :publis, '/feed.xml',
+#      :num_entries => 10
+end
