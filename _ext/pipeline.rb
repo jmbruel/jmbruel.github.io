839fb2106f60bc20247a11b1dabea5d657b159d2
Jean-Michel Bruel <jbruel@gmail.com>
2015-09-22 19:13:21 +0200

    photo

---
+++ _ext/pipeline.rb
@@ -0,0 +1,12 @@
+require 'zurb-foundation'
+require 'myTime'
+require './stylesheets/_themes/lib/functions.rb'
+
+
+Awestruct::Extensions::Pipeline.new do
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  helper MyTime
+end
