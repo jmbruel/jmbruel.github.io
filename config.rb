2bb83d99814042ac7d08df5a9c4b9e850664384e
Jean-Michel Bruel <jbruel@gmail.com>
2015-09-22 18:21:15 +0200

    merge

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ project_type = :stand_alone
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "_site/stylesheets"
-sass_dir = "stylesheets"
+css_dir = "css"
+sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascript"
+javascripts_dir = "js"
 fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
