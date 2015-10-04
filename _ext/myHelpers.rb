576fca81a626886ed7206c14a9c3b5abfbad6168
Jean-Michel Bruel <jbruel@gmail.com>
2015-10-04 21:39:12 +0200

    image_tag

--- _ext/myHelpers.rb
+++ _ext/myHelpers.rb
@@ -9,4 +9,8 @@ module MyHelpers
   def link_to(text, link)
     "<a href=#{link}>#{text}</a>"
   end
+  # usage <%= image_tag("logo-seebook.png", :alt => "[Logo GDR]", :class => "logo") %>
+  def image_tag(img, alt, classdef)
+    "<img link=#{link} alt=#{alt} class=#{classdef} />"
+  end
 end
